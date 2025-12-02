c48554b4a9af19b1e8032c236c500400e790606f
hafsa-11 <hafsa98lakhouaja@gmail.com>
2025-12-02 10:52:11 +0100

    upload

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),               
-    path("login/", views.login_view, name="login"),  
-    path("logout/", views.logout_view, name="logout"),  
-    path("register/", views.register_view, name="register"),  
-    path("profile/", views.profile_view, name="profile"),  
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("register/", views.register, name="register"),
+    path("profile/", views.profile, name="profile"),
+ 
 ]
